15bc5d4022e6bcdd2aaffcdfbf98a2c7eac4dc28
Breno Garcia <BrenoGHeat@gmail.com>
2024-05-09 14:43:59 -0300

    initial commit

---
+++ src/components/BannerSection/index.jsx
@@ -0,0 +1,29 @@
+import BannerIlustration from "../../assets/BannerFood.png";
+import styles from './style.module.css';
+
+
+export const BannerSection = () => {
+  return (
+    <section className={`${styles.bannerSection} section-padding `}>
+      <div className="container">
+        <div className={styles.flexBox}>
+          <div>
+          <h1 className="title black lg">
+            Descubra o melhor <span className="red">aplicativo de comida</span>
+          </h1>
+          <p className="paragraph black">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+            maximus tempor ullamcorper. Vivamus porta lorem eu quam ultricies,
+            non imperdiet erat elementum.
+          </p>
+          <a href="#" className="btn red">
+            Saiba Mais
+          </a>
+        </div>
+
+        <img src={BannerIlustration} alt="Comidas Gostosas" />
+        </div>
+      </div>
+    </section>
+  );
+};
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,15 @@
+
+import styles from './style.module.css';
+
+
+export const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className="container">
+        <p className="paragraph white center">
+          Todos os direitos reservados - Kenzie Academy Brasil - 2024{" "}
+        </p>
+      </div>
+    </footer>
+  );
+};
